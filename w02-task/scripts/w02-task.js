14219d454e3f9f555bcb29bb01f266461e5297df
shaylafoley <wholemotherhood@gmail.com>
2024-01-18 19:51:28 -0500

    week 2 task

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,25 +3,40 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
+let fullName = "Shayla Foley";
+const currentYear = new Date().getFullYear();
+const profilePicture = "images/profile.jpg";
 
 
 /* Step 3 - Element Variables */
 
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 
 
 /* Step 4 - Adding Content */
 
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile Image of ${fullName}`);
 
 
 
 
 /* Step 5 - Array */
-
+let favFoods = ["Chicken", "Potatoes", "Broccoli", "Ice Cream", "Cheesecake"];
+foodElement.innerHTML = favFoods;
+let newFood = "Soup";
+favFoods.push(newFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
 
 
 
