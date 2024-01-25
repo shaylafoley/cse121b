cd2ba2b6fb4c13b875d9085f22aaa526a79edd06
shaylafoley <wholemotherhood@gmail.com>
2024-01-25 13:18:52 -0500

    organize

---
+++ scripts/w03-task.js
@@ -0,0 +1,38 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add(number1, number2){
+    return number1 + number2;
+}
+function addNumbers(addNumber1, addNumber2) {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+
+    document.querySelector('#sum').value =  add(addNumber1, addNumber2);
+}
+document.querySelector('#addnumbers').addEventListener("click", addNumbers)
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
