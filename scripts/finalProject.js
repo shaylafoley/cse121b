7bfb458c63c8693ab780d627b114275a683f9055
shaylafoley <wholemotherhood@gmail.com>
2024-02-14 17:55:17 -0500

    final

---
+++ scripts/finalProject.js
@@ -0,0 +1,53 @@
+/* Final Project: Programming Tasks */
+
+/* Declare and initialize global variables */
+
+
+const url = "https://jsonplaceholder.org/users";
+let results = null;
+
+/* async getUsers Function */
+async function getUsers(url) {
+    const response = await fetch(url)
+    if (response.ok) {
+        const data = await response.json();
+displayUsers(data);
+bdayMessage(data);
+    }
+}
+
+// displayUsers function
+function displayUsers(data) {
+    const bdayList = document.querySelector('ul');
+    data.forEach(employee => {
+        const li = document.createElement('li');
+        li.textContent = `${employee.firstname} ${employee.lastname} | ${employee.birthDate}`;
+        bdayList.appendChild(li);
+            
+    });
+}
+getUsers(url);
+
+
+function bdayMessage(data) {
+    const currentMonth = new Date().getMonth();
+
+    const birthdayOutput = document.getElementById('birthdayOutput');
+    birthdayOutput.innerHTML = '';
+
+    data.forEach(user => {
+        const userBirthday = new Date(user.birthDate)
+        const userBirthMonth = userBirthday.getMonth();
+
+        if (userBirthMonth === currentMonth) {
+            var message = `${user.firstname} ${user.lastname}'s birthday is this month!`;
+            var messageNode = document.createElement('p');
+            messageNode.textContent = message;
+            birthdayOutput.appendChild(messageNode);
+        }
+        
+    });
+    
+};
+
+
